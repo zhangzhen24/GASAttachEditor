b0324c82a744c9b23c0792c0b9817b9534080ac7
Zhang Zhen <735398170@qq.com>
2024-10-11 23:41:47 +0800

    添加对GASActor的获取保护
    添加对GASActor的获取保护

--- Source/GASAttachEditor/Private/TagLookAsset/SGASLookAssetBase.cpp
+++ Source/GASAttachEditor/Private/TagLookAsset/SGASLookAssetBase.cpp
@@ -4,13 +4,17 @@
 #if WITH_EDITOR
 #include "EditorFontGlyphs.h"
 #include "EditorStyleSet.h"
+#include "Editor/EditorEngine.h"
+#include "Subsystems/AssetEditorSubsystem.h"
 #endif
 #include "GameplayTagsManager.h"
 #include "Widgets/Input/SHyperlink.h"
-#include "Widgets/Text/STextBlock.h"
-#include "Widgets/Layout/SBox.h"
 
 #define LOCTEXT_NAMESPACE "SGASAttachEditor"
+
+#if WITH_EDITOR
+extern UNREALED_API UEditorEngine* GEditor;
+#endif
 
 FGASLookAsset::~FGASLookAsset()
 {
@@ -19,7 +23,7 @@ FGASLookAsset::~FGASLookAsset()
 
 TSharedRef<FGASLookAsset> FGASLookAsset::Create(UObject* AssObj, FAbilityTriggerData InActivationTag)
 {
-	return MakeShareable(new FGASLookAsset(AssObj,InActivationTag));
+	return MakeShareable(new FGASLookAsset(AssObj, InActivationTag));
 }
 
 FText FGASLookAsset::GetTriggerSourceName() const
@@ -61,7 +65,7 @@ void SGASLookAssetTreeItem::Construct(const FArguments& InArgs, const TSharedRef
 	AbilitieAsset = WidgetInfo->GetAbilitieAsset();
 	LookAssObj = WidgetInfo->GetAbilitieAssetObj();
 	TriggerSourceName = WidgetInfo->GetTriggerSourceName();
-	SMultiColumnTableRow< TSharedRef<FGASLookAssetBase> >::Construct(SMultiColumnTableRow< TSharedRef<FGASLookAssetBase> >::FArguments().Padding(0), InOwnerTableView);
+	SMultiColumnTableRow<TSharedRef<FGASLookAssetBase>>::Construct(SMultiColumnTableRow<TSharedRef<FGASLookAssetBase>>::FArguments().Padding(0), InOwnerTableView);
 }
 
 TSharedRef<SWidget> SGASLookAssetTreeItem::GenerateWidgetForColumn(const FName& ColumnName)
@@ -85,13 +89,13 @@ TSharedRef<SWidget> SGASLookAssetTreeItem::GenerateWidgetForColumn(const FName& 
 			.VAlign(VAlign_Center)
 			.Padding(FMargin(2.0f, 0.0f))
 			[
-				
+
 				SNew(SBorder)
-				.HAlign(HAlign_Left)
-				.VAlign(VAlign_Center)
+				             .HAlign(HAlign_Left)
+				             .VAlign(VAlign_Center)
 				//.Padding(FMargin(2.0f, 0.0f))
-				.Visibility(EVisibility::SelfHitTestInvisible)
-				.BorderBackgroundColor(FSlateColor(FLinearColor(1.f, 1.f, 1.f, 0.f)))
+				             .Visibility(EVisibility::SelfHitTestInvisible)
+				             .BorderBackgroundColor(FSlateColor(FLinearColor(1.f, 1.f, 1.f, 0.f)))
 				//.ColorAndOpacity(FLinearColor(1.f,1.f,1.f,0.5f))
 				[
 					SNew(SHyperlink)
@@ -130,50 +134,50 @@ void SGASTagViewItem::Construct(const FArguments& InArgs)
 {
 	TagName = InArgs._TagName;
 	OnLookAssetDel = InArgs._OnLookAssetDel;
-	FTextBlockStyle InTextStyle = FCoreStyle::Get().GetWidgetStyle< FTextBlockStyle >("NormalText");
+	FTextBlockStyle InTextStyle = FCoreStyle::Get().GetWidgetStyle<FTextBlockStyle>("NormalText");
 	InTextStyle.ColorAndOpacity = FSlateColor(FLinearColor::White);
 
 	ChildSlot
+	[
+		SNew(SBorder)
+		.Padding(1.f)
+		.ToolTipText_Raw(this, &SGASTagViewItem::OnTipTag)
 		[
-			SNew(SBorder)
-			.Padding(1.f)
-			.ToolTipText_Raw(this, &SGASTagViewItem::OnTipTag)
-			[
-				SNew(SHorizontalBox)
+			SNew(SHorizontalBox)
 
-				+SHorizontalBox::Slot()
-				.HAlign(HAlign_Left)
-				.VAlign(VAlign_Center)
-				.AutoWidth()
-				[
-				
-					SNew(SButton)
-					.ContentPadding(FMargin(0))
+			+ SHorizontalBox::Slot()
+			.HAlign(HAlign_Left)
+			.VAlign(VAlign_Center)
+			.AutoWidth()
+			[
+
+				SNew(SButton)
+				.ContentPadding(FMargin(0))
 #if WITH_EDITOR
-					.ButtonStyle(FAppStyle::Get(), "FlatButton.Danger")
+				.ButtonStyle(FAppStyle::Get(), "FlatButton.Danger")
 #endif
-					.ForegroundColor(FSlateColor::UseForeground())
-					.OnClicked(this, &SGASTagViewItem::OnRemoveTagClicked)
-					[
-						SNew(STextBlock)
+				.ForegroundColor(FSlateColor::UseForeground())
+				.OnClicked(this, &SGASTagViewItem::OnRemoveTagClicked)
+				[
+					SNew(STextBlock)
 #if WITH_EDITOR
-						.Font(FAppStyle::Get().GetFontStyle("FontAwesome.9"))
-						.Text(FEditorFontGlyphs::Times)
+					                .Font(FAppStyle::Get().GetFontStyle("FontAwesome.9"))
+					                .Text(FEditorFontGlyphs::Times)
 #else
 						.Text(FText::FromString("x"))
 #endif
-					]
-			
 				]
-				+SHorizontalBox::Slot()
-				.AutoWidth()
-				[
-					SNew(STextBlock)
-					.Text(FText::FromString(TagName.Get().ToString()))
-					.TextStyle(&InTextStyle)
-				]
+
 			]
-		];
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+				SNew(STextBlock)
+				.Text(FText::FromString(TagName.Get().ToString()))
+				.TextStyle(&InTextStyle)
+			]
+		]
+	];
 }
 
 FReply SGASTagViewItem::OnRemoveTagClicked()
@@ -188,7 +192,9 @@ FReply SGASTagViewItem::OnRemoveTagClicked()
 FText SGASTagViewItem::OnTipTag() const
 {
 #if WITH_EDITOR
-	FString OutComment; FName OutTagSource; bool bOutIsTagExplicit, bOutIsRestrictedTag, bOutAllowNonRestrictedChildren;
+	FString OutComment;
+	FName OutTagSource;
+	bool bOutIsTagExplicit, bOutIsRestrictedTag, bOutAllowNonRestrictedChildren;
 	if (UGameplayTagsManager::Get().GetTagEditorData(*TagName.Get().ToString(), OutComment, OutTagSource, bOutIsTagExplicit, bOutIsRestrictedTag, bOutAllowNonRestrictedChildren))
 	{
 		// 是否有注释
@@ -204,4 +210,3 @@ FText SGASTagViewItem::OnTipTag() const
 }
 
 #undef LOCTEXT_NAMESPACE
-
